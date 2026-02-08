153af06d3335ffc81b892b3181765824485d1d68
Andrei Kot <andreikott2005@mai.ru>
2026-02-08 23:25:00 +0300

    Выполнил 1 задание 7 урока

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
         int ghoulTearsCount = 0;        //Слезы вурдалака
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        boolean isCanCook = false;
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+ System.out.println(" Введите кол-во жабьих глаз");
+ toadEyesCount = new Scanner(System.in).nextInt();
+
+        System.out.println(" Введите кол-во cлез вурдалака");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+
+        System.out.println(" Введите кол-во костей вороны");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+
+        System.out.println(" Введите кол-во пельменей");
+       dumplingsCount = new Scanner(System.in).nextInt();
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -14,5 +28,25 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+        if ( toadEyesCount >= 3 &&  ravenBonesCount  >= 1 ) {
+            System.out.println(" Вы можете изготовить Элексир зоркости");
+            isCanCook = true;
+    }
+        if ( ravenBonesCount >= 2 &&  dumplingsCount  >= 4 ) {
+            System.out.println(" Вы можете изготовить Элексир стойкости");
+            isCanCook = true;
+        }
+        if ( ghoulTearsCount >= 7 &&  dumplingsCount  >= 1 && toadEyesCount >= 2 ) {
+            System.out.println(" Вы можете изготовить Элексир скрытности");
+            isCanCook = true;
+        }
+        if ( ghoulTearsCount >= 7 &&  dumplingsCount  >= 10 && ravenBonesCount >=3 ) {
+            System.out.println(" Вы можете изготовить Запретный элексир");
+            isCanCook = true;
+        }
+        if (!isCanCook){
+            System.out.println("Вы не можеите приготовить не один элексир ");
+
+        }
     }
 }
